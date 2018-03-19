20663210931f21549b1136f441696f3eb872d2ba
MilanPrpa <milanprpa25@gmail.com>
2018-03-19 14:26:17 -0700

    initial weather app prototype

---
+++ app.js
@@ -0,0 +1,27 @@
+"use strict";
+
+searchButton.addEventListener('click', searchWeather);
+
+function searchWeather() {
+	var cityName = searchCity.value;
+	if (cityName.trim().length === 0) {
+		return alert('Please enter a city name!')
+	}
+	var http = new XMLHttpRequest();
+	var apiKey = '1e99098a64ad5ee3e606f4eefb9b4394';
+	var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey;
+	var method = 'GET';
+
+	http.open(method, url);
+	http.onreadystatechange = function() {
+		if (http.readyState ===  XMLHttpRequest.DONE && http.status === 200) {
+			var data = JSON.parse(http.responseText);
+			var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+			weatherData.temperature = data.main.temp;
+			console.log(weatherData);
+		} else if (http.readyState ===  XMLHttpRequest.DONE ) {
+			alert('There was an error');
+		}
+	};
+	http.send();
+}
---
+++ close.js
@@ -0,0 +1,14 @@
+
+function generator(input) {
+	var number = input;
+	return function() {
+		return number * 2;
+	}
+}
+
+var calc = generator(900);
+
+console.log(calc());
+
+
+
